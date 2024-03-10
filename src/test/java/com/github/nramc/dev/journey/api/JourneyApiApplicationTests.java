9bff1548d1e639df7ef109e45e3e7bb8af6307a6
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-03-10 15:29:19 +0100

    [feat] #1 spring actuator added to monitor application health

--- src/test/java/com/github/nramc/dev/journey/api/JourneyApiApplicationTests.java
+++ src/test/java/com/github/nramc/dev/journey/api/JourneyApiApplicationTests.java
@@ -5,15 +5,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class JourneyApiApplicationTests {
     @Autowired
     private ApplicationContext applicationContext;
+    @Autowired
+    private WebTestClient webTestClient;
 
     @Test
     void contextLoads() {
         Assertions.assertNotNull(applicationContext);
+    }
+
+    @Test
+    void testHealthCheckEndpoint() {
+        webTestClient.get().uri("/actuator/health").exchange()
+                .expectStatus().isOk();
     }
 
 }
