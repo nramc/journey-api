bbe1c2b60096d8f5f31a54ed2e629cca8be82379
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-03-10 22:16:26 +0100

    [feat] #5 mongo db base setup added
     - docker-compose.yml added for local development
     -  profiling added
     - connection mongo db established

--- src/test/java/com/github/nramc/dev/journey/api/JourneyApiApplicationTests.java
+++ src/test/java/com/github/nramc/dev/journey/api/JourneyApiApplicationTests.java
@@ -4,11 +4,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
+@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class JourneyApiApplicationTests {
+    @Container
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+
     @Autowired
     private ApplicationContext applicationContext;
     @Autowired
