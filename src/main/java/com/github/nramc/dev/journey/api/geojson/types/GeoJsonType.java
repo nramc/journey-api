537a32a88db43731d18e0b64bf9a4e3567129307
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-03-14 21:11:42 +0100

    [feat] #7 geojson data modeling
     - GeoJsonType.java added
    
     -

---
+++ src/main/java/com/github/nramc/dev/journey/api/geojson/types/GeoJsonType.java
@@ -0,0 +1,23 @@
+package com.github.nramc.dev.journey.api.geojson.types;
+
+import lombok.Getter;
+
+@Getter
+public enum GeoJsonType {
+    POINT("Point"),
+    MULTI_POINT("MultiPoint"),
+    LINE_STRING("LineString"),
+    MULTI_LINE_STRING("MultiLineString"),
+    POLYGON("Polygon"),
+    MULTI_POLYGON("MultiPolygon"),
+    GEOMETRY_COLLECTION("GeometryCollection"),
+    FEATURE("Feature"),
+    FEATURE_COLLECTION("FeatureCollection");
+
+    private final String type;
+
+    GeoJsonType(String type) {
+        this.type = type;
+    }
+
+}
