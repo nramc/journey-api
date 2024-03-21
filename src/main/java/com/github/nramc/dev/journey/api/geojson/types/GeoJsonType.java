1e792007f05fa6acb6fcb8f7e60742d836bb4a1c
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-03-21 10:26:59 +0100

    [feat] #7 geojson data modeling added - type constant values added

--- src/main/java/com/github/nramc/dev/journey/api/geojson/types/GeoJsonType.java
+++ src/main/java/com/github/nramc/dev/journey/api/geojson/types/GeoJsonType.java
@@ -5,21 +5,37 @@ import lombok.Getter;
 
 @Getter
 public enum GeoJsonType {
-    POINT("Point"),
-    MULTI_POINT("MultiPoint"),
-    LINE_STRING("LineString"),
-    MULTI_LINE_STRING("MultiLineString"),
-    POLYGON("Polygon"),
-    MULTI_POLYGON("MultiPolygon"),
-    GEOMETRY_COLLECTION("GeometryCollection"),
-    FEATURE("Feature"),
-    FEATURE_COLLECTION("FeatureCollection");
+    POINT(Constants.POINT_VALUE),
+    MULTI_POINT(Constants.MULTI_POINT_VALUE),
+    LINE_STRING(Constants.LINE_STRING_VALUE),
+    MULTI_LINE_STRING(Constants.MULTI_LINE_STRING_VALUE),
+    POLYGON(Constants.POLYGON_VALUE),
+    MULTI_POLYGON(Constants.MULTI_POLYGON_VALUE),
+    GEOMETRY_COLLECTION(Constants.GEOMETRY_COLLECTION_VALUE),
+    FEATURE(Constants.FEATURE_VALUE),
+    FEATURE_COLLECTION(Constants.FEATURE_COLLECTION_VALUE);
 
     @JsonValue
     private final String type;
 
     GeoJsonType(String type) {
         this.type = type;
+    }
+
+    public static class Constants {
+        private Constants() {
+        }
+
+        public static final String POINT_VALUE = "Point";
+        public static final String MULTI_POINT_VALUE = "MultiPoint";
+        public static final String LINE_STRING_VALUE = "LineString";
+        public static final String MULTI_LINE_STRING_VALUE = "MultiLineString";
+        public static final String POLYGON_VALUE = "Polygon";
+        public static final String MULTI_POLYGON_VALUE = "MultiPolygon";
+        public static final String GEOMETRY_COLLECTION_VALUE = "GeometryCollection";
+        public static final String FEATURE_VALUE = "Feature";
+        public static final String FEATURE_COLLECTION_VALUE = "FeatureCollection";
+
     }
 
 }
