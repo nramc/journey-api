9a8763132dd4a1d705b912de73c0feb5ab881f76
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-03-24 17:29:33 +0100

    [feat] #12 create journey feature
     - basic exceptions and rest handler added

---
+++ src/main/java/com/github/nramc/dev/journey/api/web/exceptions/GlobalRestExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.github.nramc.dev.journey.api.web.exceptions;
+
+
+import jakarta.annotation.Nonnull;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalRestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({BusinessException.class})
+    protected ProblemDetail handleValidationFailure(BusinessException ex) {
+        log.warn(ex.getMessage(), ex);
+        return ex.getProblemDetail();
+    }
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    protected ProblemDetail handleValidationFailure(ConstraintViolationException ex) {
+        log.warn(ex.getMessage(), ex);
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler({NonTechnicalException.class, RuntimeException.class, Exception.class})
+    protected ProblemDetail handleNonTechnicalExceptions(Exception ex) {
+        log.warn(ex.getMessage(), ex);
+        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.getMessage());
+    }
+
+    @ExceptionHandler({TechnicalException.class})
+    protected ProblemDetail handleTechnicalException(Exception ex) {
+        log.error(ex.getMessage(), ex);
+        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(@Nonnull MethodArgumentNotValidException ex,
+                                                                  @Nonnull HttpHeaders headers,
+                                                                  @Nonnull HttpStatusCode status,
+                                                                  @Nonnull WebRequest request) {
+
+        return ResponseEntity.badRequest().body(ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage()));
+    }
+
+}
+
---
+++ src/main/java/com/github/nramc/dev/journey/api/web/exceptions/TechnicalException.java
@@ -0,0 +1,7 @@
+package com.github.nramc.dev.journey.api.web.exceptions;
+
+public class TechnicalException extends RuntimeException {
+    public TechnicalException(String message) {
+        super(message);
+    }
+}
