842c1936faac2462f322f40572f7bb451a03edbb
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-03-27 09:19:36 +0100

    [feat] #20 add createdDate and journeyDate

--- src/main/java/com/github/nramc/dev/journey/api/web/resources/rest/find/FindJourneyResponse.java
+++ src/main/java/com/github/nramc/dev/journey/api/web/resources/rest/find/FindJourneyResponse.java
@@ -1,11 +1,13 @@
 package com.github.nramc.dev.journey.api.web.resources.rest.find;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.github.nramc.commons.geojson.domain.Geometry;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Builder(toBuilder = true)
@@ -19,5 +21,7 @@ public record FindJourneyResponse(
         @NotBlank String country,
         @NotEmpty List<String> tags,
         @NotBlank String thumbnail,
-        @NotNull Geometry location) {
+        @NotNull Geometry location,
+        @NotNull @JsonFormat(pattern = "yyyy-MM-dd") LocalDate journeyDate,
+        @NotNull @JsonFormat(pattern = "yyyy-MM-dd") LocalDate createdDate) {
 }
