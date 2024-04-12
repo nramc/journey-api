699805780ebc3979282eeff73b7203f6255185d1
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-04-12 16:33:58 +0200

    [feat] secret stored securely in memory

--- src/test/java/com/github/nramc/dev/journey/api/config/security/WebSecurityTestConfig.java
+++ src/test/java/com/github/nramc/dev/journey/api/config/security/WebSecurityTestConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @TestConfiguration
@@ -13,17 +14,16 @@ public class WebSecurityTestConfig {
 
     @Bean
     @Primary
-    public UserDetailsService testUserDetailsService() {
+    public UserDetailsService testUserDetailsService(PasswordEncoder passwordEncoder) {
         // The builder will ensure the passwords are encoded before saving in memory
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        UserDetails user = users
+        UserDetails user = User.builder()
                 .username("test-user")
-                .password("test-password")
+                .password(passwordEncoder.encode("test-password"))
                 .roles("USER")
                 .build();
-        UserDetails admin = users
+        UserDetails admin = User.builder()
                 .username("test-admin")
-                .password("test-password")
+                .password(passwordEncoder.encode("test-password"))
                 .roles("USER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
