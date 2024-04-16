64a14903198e4780e980b7e0fc3b3fb544db8167
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-04-16 23:03:16 +0200

    [feat] login endpoint updated and response dto added

--- src/main/java/com/github/nramc/dev/journey/api/web/resources/Resources.java
+++ src/main/java/com/github/nramc/dev/journey/api/web/resources/Resources.java
@@ -5,7 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Resources {
     public static final String HOME = "";
-    public static final String LOGIN = "/auth/login";
+    public static final String LOGIN = "/rest/login";
     public static final String CREATE_JOURNEY = "/rest/journey";
     public static final String FIND_JOURNEY = "/rest/journey/{id}";
     public static final String FIND_JOURNEYS = "/rest/journeys";
