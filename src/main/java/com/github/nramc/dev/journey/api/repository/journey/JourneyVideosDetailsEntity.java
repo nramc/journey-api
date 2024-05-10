3195e19075d2ebd906968780ea67b56985e98cde
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-05-10 18:02:14 +0200

    fix: no argument and all argument constructor added for fixing mongodb deserialization issue

--- src/main/java/com/github/nramc/dev/journey/api/repository/journey/JourneyVideosDetailsEntity.java
+++ src/main/java/com/github/nramc/dev/journey/api/repository/journey/JourneyVideosDetailsEntity.java
@@ -1,12 +1,16 @@
 package com.github.nramc.dev.journey.api.repository.journey;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class JourneyVideosDetailsEntity {
     private List<JourneyVideoDetailEntity> videos;
 
