702da6a0a8d8fd980acfddf3f591eb7f4d7c9fe0
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-05-11 22:44:26 +0200

    doc: Swagger Header updated with contact details

--- src/main/java/com/github/nramc/dev/journey/api/config/ApplicationContextConfig.java
+++ src/main/java/com/github/nramc/dev/journey/api/config/ApplicationContextConfig.java
@@ -3,7 +3,9 @@ package com.github.nramc.dev.journey.api.config;
 import com.github.nramc.dev.journey.api.config.security.JwtProperties;
 import com.github.nramc.dev.journey.api.web.resources.rest.auth.jwt.JwtGenerator;
 import com.github.nramc.dev.journey.api.web.resources.rest.journeys.update.validator.JourneyValidator;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import jakarta.validation.Validator;
 import org.springdoc.core.models.GroupedOpenApi;
@@ -36,7 +38,18 @@ public class ApplicationContextConfig {
                     return operation;
                 })
                 .packagesToScan("com.github.nramc.dev.journey.api.web.resources.rest.api")
-                .addOpenApiCustomizer(openApi -> openApi.info(new Info().title("Journey API").version(appVersion)))
+                .addOpenApiCustomizer(openApi -> openApi.info(new Info()
+                                .title("Journey API")
+                                .version(appVersion)
+                                .description("This is Backend For Frontend(BFF) service created using SpringBoot and OAuth2 Framework for Journey Single Page Application(SPA).")
+                                .license(new License().name("Apache 2.0"))
+                                .contact(new Contact()
+                                        .name("Ramachandran Nellaiyappan")
+                                        .email("ramachandrannellai@gmail.com")
+                                        .url("https://nramc.github.io/journeys")
+                                )
+                        )
+                )
                 .build();
     }
 
