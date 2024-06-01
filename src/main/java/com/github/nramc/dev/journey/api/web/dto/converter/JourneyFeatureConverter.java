53c0e7527fee4a401c7b6eef3f17c6f912263628
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-06-01 16:41:10 +0200

    feat: geo-location used for geo json feature #122

--- src/main/java/com/github/nramc/dev/journey/api/web/dto/converter/JourneyFeatureConverter.java
+++ src/main/java/com/github/nramc/dev/journey/api/web/dto/converter/JourneyFeatureConverter.java
@@ -1,24 +1,18 @@
 package com.github.nramc.dev.journey.api.web.dto.converter;
 
 import com.github.nramc.commons.geojson.domain.Feature;
-import com.github.nramc.commons.geojson.domain.Geometry;
 import com.github.nramc.dev.journey.api.repository.journey.JourneyEntity;
-import com.github.nramc.dev.journey.api.repository.journey.JourneyExtendedEntity;
-import com.github.nramc.dev.journey.api.repository.journey.JourneyGeoDetailsEntity;
 import lombok.experimental.UtilityClass;
 import org.apache.commons.collections4.map.HashedMap;
 
 import java.io.Serializable;
 import java.util.Map;
-import java.util.Optional;
 
 @UtilityClass
 public class JourneyFeatureConverter {
 
     public static Feature toFeature(JourneyEntity entity) {
-        Geometry geometry = Optional.of(entity).map(JourneyEntity::getExtended).map(JourneyExtendedEntity::getGeoDetails)
-                .map(JourneyGeoDetailsEntity::getGeoJson).orElse(entity.getLocation());
-        return Feature.of(entity.getId(), geometry, toProperties(entity));
+        return Feature.of(entity.getId(), entity.getLocation(), toProperties(entity));
     }
 
     private static Map<String, Serializable> toProperties(JourneyEntity entity) {
