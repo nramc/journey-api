8562a819494befeb763d0ad94691f85f74c9cc3e
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-06-08 19:55:00 +0200

    feat!: migration feature added

--- src/main/java/com/github/nramc/dev/journey/api/migration/journeys/HelloWorld.java
+++ src/main/java/com/github/nramc/dev/journey/api/migration/journeys/HelloWorld.java
@@ -1,49 +1,24 @@
 package com.github.nramc.dev.journey.api.migration.journeys;
 
-import com.mongodb.MongoException;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.UpdateOptions;
-import com.mongodb.client.model.Updates;
-import com.mongodb.client.result.UpdateResult;
+import com.github.nramc.dev.journey.api.repository.journey.JourneyEntity;
+import com.github.nramc.dev.journey.api.repository.journey.JourneyRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.bson.Document;
-import org.bson.conversions.Bson;
+
+import java.util.List;
 
 @Slf4j
-public class HelloWorld {
+public class HelloWorld implements Runnable {
+    private final JourneyRepository journeyRepository;
 
-    public static void main(String[] args) {
-        // Replace the uri string with your MongoDB deployment's connection string
-        String uri = "<connection string uri>";
-
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
-
-            MongoCollection<Document> collection = database.getCollection("movies");
-
-            Document query = new Document().append("title", "Update Me");
-
-            // Creates instructions to update the values of three document fields
-            Bson updates = Updates.combine(
-                    Updates.set("runtime", 99),
-                    Updates.addToSet("genres", "Sports"),
-                    Updates.currentTimestamp("lastUpdated"));
-
-            // Instructs the driver to insert a new document if none match the query
-            UpdateOptions options = new UpdateOptions().upsert(true);
-
-            try {
-                UpdateResult result = collection.updateOne(query, updates, options);
-                log.info("Number of rows matched: {}", result.getModifiedCount());
-                log.info("Modified rows count: {}", result.getModifiedCount());
-                log.info("Upserted id: {}", result.getUpsertedId());
-            } catch (MongoException ex) {
-                log.error("Migration Failed", ex);
-            }
-        }
+    public HelloWorld(JourneyRepository journeyRepository) {
+        this.journeyRepository = journeyRepository;
     }
 
+    @Override
+    public void run() {
+        List<JourneyEntity> entities = journeyRepository.findAll().stream().filter(journey -> journey.getCategory().startsWith("Marriage "))
+                .map(journey -> journey.toBuilder().category("Marriage Alaparaigal").build()).toList();
+        journeyRepository.saveAll(entities);
+        log.info("No of journeys exists: {}", entities.size());
+    }
 }
