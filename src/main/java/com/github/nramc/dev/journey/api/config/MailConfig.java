7eba215d6a2ba5d8b61a88fb5fca21e7bccc2caf
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-06-17 21:36:36 +0200

    feat: email code template added
     - unit test failure fixed by excluding auto config for mail

--- src/main/java/com/github/nramc/dev/journey/api/config/MailConfig.java
+++ src/main/java/com/github/nramc/dev/journey/api/config/MailConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -13,6 +14,7 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
+@Profile("!test")
 public class MailConfig {
 
     @Bean("thymeleafEmailTemplateResolver")
