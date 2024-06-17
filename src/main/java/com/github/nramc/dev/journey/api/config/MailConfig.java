c1a95c3900b05c4cd9abb72b26faf44026d485ed
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-06-17 21:36:36 +0200

    feat: email code template added

--- src/main/java/com/github/nramc/dev/journey/api/config/MailConfig.java
+++ src/main/java/com/github/nramc/dev/journey/api/config/MailConfig.java
@@ -1,8 +1,13 @@
 package com.github.nramc.dev.journey.api.config;
 
+import com.github.nramc.dev.journey.api.services.MailService;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
@@ -17,6 +22,7 @@ public class MailConfig {
         templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode("HTML");
         templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setCacheable(false);
         return templateResolver;
     }
 
@@ -25,7 +31,18 @@ public class MailConfig {
             @Qualifier("thymeleafEmailTemplateResolver") ITemplateResolver thymeleafEmailTemplateResolver) {
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(thymeleafEmailTemplateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+
         return templateEngine;
+    }
+
+    @Bean
+    public MailService mailService(
+            @Value("classpath:/assets/logo.png") Resource logoResource,
+            JavaMailSender emailSender,
+            SpringTemplateEngine templateEngine
+    ) {
+        return new MailService(logoResource, emailSender, templateEngine);
     }
 
 }
--- src/main/java/com/github/nramc/dev/journey/api/services/MailService.java
+++ src/main/java/com/github/nramc/dev/journey/api/services/MailService.java
@@ -4,19 +4,19 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.util.Map;
 
-@Service
-@RequiredArgsConstructor
 @Slf4j
+@RequiredArgsConstructor
 public class MailService {
+    private final Resource logoResource;
     private final JavaMailSender emailSender;
     private final SpringTemplateEngine templateEngine;
 
@@ -31,10 +31,10 @@ public class MailService {
 
     public void sendEmailUsingTemplate(String template, String to, String subject, Map<String, Object> variables)
             throws MessagingException {
+        Context context = new Context();
+        context.setVariables(variables);
 
-        Context thymeleafContext = new Context();
-        thymeleafContext.setVariables(variables);
-        String htmlBody = templateEngine.process(template, thymeleafContext);
+        String htmlBody = templateEngine.process(template, context);
 
         sendHtmlEmail(to, subject, htmlBody);
         log.info("Email using template[{}] has been sent successfully", template);
@@ -46,6 +46,7 @@ public class MailService {
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(htmlBody, true);
+        helper.addInline("logo.png", logoResource);
         emailSender.send(message);
     }
 }
