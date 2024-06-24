555d9d5ae7e1f5cd34715092c414934a144c5460
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-06-24 20:57:16 +0200

    feat: Email Code Verification
     - base setup added for verification
     - mock mail service added for test
     - EmailAddress model added with obfuscation utilities
     - Confirmation code saved in database
     - Confirmation code service created for better maintainability

--- src/main/java/com/github/nramc/dev/journey/api/models/core/ConfirmationCodeType.java
+++ src/main/java/com/github/nramc/dev/journey/api/models/core/ConfirmationCodeType.java
@@ -1,5 +1,6 @@
 package com.github.nramc.dev.journey.api.models.core;
 
 public enum ConfirmationCodeType {
-    EMAIL_CODE
+    EMAIL_CODE,
+    EMAIL_TOKEN
 }
---
+++ src/test/java/com/github/nramc/dev/journey/api/services/email/EmailCodeValidatorTest.java
@@ -0,0 +1,84 @@
+package com.github.nramc.dev.journey.api.services.email;
+
+import com.github.nramc.dev.journey.api.models.core.ConfirmationCodeType;
+import com.github.nramc.dev.journey.api.repository.auth.AuthUser;
+import com.github.nramc.dev.journey.api.repository.security.ConfirmationCodeEntity;
+import com.github.nramc.dev.journey.api.repository.security.ConfirmationCodeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static com.github.nramc.dev.journey.api.services.confirmationcode.ConfirmationUseCase.VERIFY_EMAIL_ADDRESS;
+import static com.github.nramc.dev.journey.api.services.email.EmailCodeValidator.EMAIL_CODE_VALIDITY_MINUTES;
+import static com.github.nramc.dev.journey.api.web.resources.rest.users.UsersData.AUTH_USER;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class EmailCodeValidatorTest {
+    private static final EmailCode VALID_CODE = EmailCode.valueOf(747469);
+    private static final AuthUser VALID_USER = AUTH_USER.toBuilder().build();
+    private static final ConfirmationCodeEntity VALID_CODE_ENTITY = ConfirmationCodeEntity.builder()
+            .id("ecc76991-0137-4152-b3b2-efce70a37ed0")
+            .isActive(true)
+            .username(VALID_USER.getUsername())
+            .type(ConfirmationCodeType.EMAIL_CODE)
+            .code(VALID_CODE.code())
+            .receiver(VALID_USER.getEmailAddress())
+            .useCase(VERIFY_EMAIL_ADDRESS)
+            .createdAt(LocalDateTime.now())
+            .build();
+    @Mock
+    private ConfirmationCodeRepository codeRepository;
+    private EmailCodeValidator emailCodeValidator;
+
+    @BeforeEach
+    void setup() {
+        emailCodeValidator = new EmailCodeValidator(codeRepository);
+    }
+
+    @Test
+    void isValid_whenCodeValid_shouldReturnTrue() {
+        when(codeRepository.findByUsernameAndCode(VALID_USER.getUsername(), VALID_CODE.code())).thenReturn(VALID_CODE_ENTITY);
+        assertTrue(emailCodeValidator.isValid(VALID_CODE, VALID_USER));
+    }
+
+    @Test
+    void isValid_whenCodeNotExists_shouldReturnFalse() {
+        assertFalse(emailCodeValidator.isValid(EmailCode.valueOf(222222), VALID_USER));
+    }
+
+    @Test
+    void isValid_whenCodeNotActive_shouldReturnFalse() {
+        when(codeRepository.findByUsernameAndCode(VALID_USER.getUsername(), VALID_CODE.code()))
+                .thenReturn(VALID_CODE_ENTITY.toBuilder().isActive(false).build());
+        assertFalse(emailCodeValidator.isValid(VALID_CODE, VALID_USER));
+    }
+
+    @Test
+    void isValid_whenReceivedEmailAddressNotMatched_shouldReturnFalse() {
+        when(codeRepository.findByUsernameAndCode(VALID_USER.getUsername(), VALID_CODE.code()))
+                .thenReturn(VALID_CODE_ENTITY.toBuilder().receiver("email-changed-after-code-generated@gmail.com").build());
+        assertFalse(emailCodeValidator.isValid(VALID_CODE, VALID_USER));
+    }
+
+    @Test
+    void isValid_whenConfirmationTypeNotMatched_shouldReturnFalse() {
+        when(codeRepository.findByUsernameAndCode(VALID_USER.getUsername(), VALID_CODE.code()))
+                .thenReturn(VALID_CODE_ENTITY.toBuilder().type(ConfirmationCodeType.EMAIL_TOKEN).build());
+        assertFalse(emailCodeValidator.isValid(VALID_CODE, VALID_USER));
+    }
+
+    @Test
+    void isValid_whenCodeExpired_shouldReturnFalse() {
+        when(codeRepository.findByUsernameAndCode(VALID_USER.getUsername(), VALID_CODE.code()))
+                .thenReturn(VALID_CODE_ENTITY.toBuilder().createdAt(LocalDateTime.now().minusMinutes(EMAIL_CODE_VALIDITY_MINUTES + 1)).build());
+        assertFalse(emailCodeValidator.isValid(VALID_CODE, VALID_USER));
+    }
+
+}
