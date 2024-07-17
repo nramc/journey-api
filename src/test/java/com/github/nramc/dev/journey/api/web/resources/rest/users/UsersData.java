77850e9bdee94ac3f85f76cc1d162053b1a961a1
Ram <ramachandrannellai@gmail.com>
2024-07-17 21:29:31 +0200

    feat: tests coverage improved for TOTP Service

--- src/test/java/com/github/nramc/dev/journey/api/web/resources/rest/users/UsersData.java
+++ src/test/java/com/github/nramc/dev/journey/api/web/resources/rest/users/UsersData.java
@@ -4,6 +4,7 @@ import com.github.nramc.dev.journey.api.models.core.SecurityAttributeType;
 import com.github.nramc.dev.journey.api.repository.auth.AuthUser;
 import com.github.nramc.dev.journey.api.web.dto.user.security.UserSecurityAttribute;
 import lombok.experimental.UtilityClass;
+import org.bson.types.ObjectId;
 
 import java.time.LocalDate;
 import java.util.Set;
@@ -13,6 +14,7 @@ import static com.github.nramc.dev.journey.api.security.Role.AUTHENTICATED_USER;
 @UtilityClass
 public class UsersData {
     public static final AuthUser AUTH_USER = AuthUser.builder()
+            .id(new ObjectId("665b1b94bd24ff59695e1d04"))
             .username("test-user")
             .password("test")
             .name("Test User")
