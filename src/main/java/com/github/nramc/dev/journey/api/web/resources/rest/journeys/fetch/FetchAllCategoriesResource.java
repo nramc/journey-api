e9205819170ab2b55fbd786c771f0ab45c5f9236
Ram <ramachandrannellai@gmail.com>
2024-07-17 22:05:49 +0200

    feat: OpenAPI doc specification updated to provide better clarity and readability

--- src/main/java/com/github/nramc/dev/journey/api/web/resources/rest/journeys/fetch/FetchAllCategoriesResource.java
+++ src/main/java/com/github/nramc/dev/journey/api/web/resources/rest/journeys/fetch/FetchAllCategoriesResource.java
@@ -6,7 +6,6 @@ import com.github.nramc.dev.journey.api.web.resources.rest.doc.RestDocCommonResp
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
