e846e06943af2be9e55f40ac6943b934a5f65f8b
Ram <ramachandrannellai@gmail.com>
2024-07-22 21:02:37 +0200

    feat: resource added to enable/disable mfa feature

--- src/main/java/com/github/nramc/dev/journey/api/web/resources/Resources.java
+++ src/main/java/com/github/nramc/dev/journey/api/web/resources/Resources.java
@@ -32,6 +32,7 @@ public class Resources {
     public static final String DELETE_MY_ACCOUNT = "/rest/my-account";
     public static final String CHANGE_MY_PASSWORD = "/rest/my-account/changePassword";
     public static final String UPDATE_MY_ACCOUNT = "/rest/my-account";
+    public static final String MY_SECURITY_MFA = "/rest/my-account/securityAttribute/mfa";
     public static final String MY_SECURITY_ATTRIBUTE_EMAIL = "/rest/my-account/securityAttribute/emailAddress";
     public static final String MY_SECURITY_ATTRIBUTE_TOTP = "/rest/my-account/securityAttribute/totp";
     public static final String MY_SECURITY_ATTRIBUTE_TOTP_STATUS = "/rest/my-account/securityAttribute/totp/status";
