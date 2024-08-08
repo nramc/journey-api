943eed8cf8d79754d752d1f93d4f21ac4ad004a6
Ram <ramachandrannellai@gmail.com>
2024-08-08 21:56:52 +0200

    feat: admin notification feature added

---
+++ src/main/java/com/github/nramc/dev/journey/api/core/usecase/notification/EmailNotificationUseCase.java
@@ -0,0 +1,23 @@
+package com.github.nramc.dev.journey.api.core.usecase.notification;
+
+import com.github.nramc.dev.journey.api.gateway.MailService;
+import com.github.nramc.dev.journey.api.repository.auth.AuthUser;
+import com.github.nramc.dev.journey.api.web.resources.rest.auth.AuthUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class EmailNotificationUseCase {
+    private static final String SUBJECT_PREFIX = "Notification: ";
+    private final MailService mailService;
+    private final AuthUserDetailsService authUserDetailsService;
+
+    public void notifyAdmin(String notificationText) {
+        List<AuthUser> admins = authUserDetailsService.findAllAdministratorUsers();
+        List<String> adminEmailAddresses = CollectionUtils.emptyIfNull(admins).stream().map(AuthUser::getUsername).toList();
+        mailService.sendSimpleEmail(adminEmailAddresses, SUBJECT_PREFIX + notificationText, SUBJECT_PREFIX + notificationText);
+    }
+
+}
---
+++ src/test/java/com/github/nramc/dev/journey/api/core/usecase/notification/EmailNotificationUseCaseTest.java
@@ -0,0 +1,48 @@
+package com.github.nramc.dev.journey.api.core.usecase.notification;
+
+import com.github.nramc.dev.journey.api.gateway.MailService;
+import com.github.nramc.dev.journey.api.web.resources.rest.auth.AuthUserDetailsService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static com.github.nramc.dev.journey.api.config.TestConfig.ADMIN_USER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.assertArg;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class EmailNotificationUseCaseTest {
+    @Mock
+    private MailService mailService;
+    @Mock
+    private AuthUserDetailsService authUserDetailsService;
+    @InjectMocks
+    private EmailNotificationUseCase emailNotificationUseCase;
+
+    @Test
+    void textContext() {
+        assertThat(mailService).isNotNull();
+        assertThat(emailNotificationUseCase).isNotNull();
+    }
+
+    @Test
+    void notifyAdmin_whenAdminNotificationTriggered_shouldSendNotificationToAllAvailableAdmins() {
+        when(authUserDetailsService.findAllAdministratorUsers()).thenReturn(List.of(ADMIN_USER));
+
+        emailNotificationUseCase.notifyAdmin("Signup Completed");
+
+        verify(mailService).sendSimpleEmail(
+                assertArg(emails -> assertThat(emails).isNotNull().hasSize(1).contains(ADMIN_USER.getUsername())),
+                eq("Notification: Signup Completed"),
+                eq("Notification: Signup Completed")
+        );
+    }
+
+}
