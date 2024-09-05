ccdb9c41da39a9ef15a457cc3e03d0b02a5ac30d
Ram <ramachandrannellai@gmail.com>
2024-09-05 23:51:59 +0200

    feat: DeleteJourneyResourceTest refactored with MockMvcTest

--- src/test/java/com/github/nramc/dev/journey/api/web/resources/rest/journeys/delete/DeleteJourneyResourceTest.java
+++ src/test/java/com/github/nramc/dev/journey/api/web/resources/rest/journeys/delete/DeleteJourneyResourceTest.java
@@ -1,16 +1,17 @@
 package com.github.nramc.dev.journey.api.web.resources.rest.journeys.delete;
 
-import com.github.nramc.dev.journey.api.config.TestContainersConfiguration;
+import com.github.nramc.dev.journey.api.config.ApplicationProperties;
 import com.github.nramc.dev.journey.api.config.security.Visibility;
+import com.github.nramc.dev.journey.api.config.security.WebSecurityConfig;
+import com.github.nramc.dev.journey.api.config.security.WebSecurityTestConfig;
+import com.github.nramc.dev.journey.api.gateway.cloudinary.CloudinaryService;
 import com.github.nramc.dev.journey.api.repository.journey.JourneyEntity;
 import com.github.nramc.dev.journey.api.repository.journey.JourneyRepository;
-import com.github.nramc.dev.journey.api.gateway.cloudinary.CloudinaryService;
 import com.github.nramc.dev.journey.api.web.resources.Resources;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -26,28 +27,26 @@ import static com.github.nramc.dev.journey.api.config.security.Role.Constants.GU
 import static com.github.nramc.dev.journey.api.config.security.Role.Constants.MAINTAINER;
 import static com.github.nramc.dev.journey.api.web.resources.rest.journeys.JourneyData.JOURNEY_EXTENDED_ENTITY;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Import(TestContainersConfiguration.class)
-@ActiveProfiles({"test"})
-@AutoConfigureMockMvc
+@WebMvcTest(DeleteJourneyResource.class)
+@Import({WebSecurityConfig.class, WebSecurityTestConfig.class})
+@ActiveProfiles({"prod", "test"})
+@EnableConfigurationProperties({ApplicationProperties.class})
+@MockBean({JourneyRepository.class, CloudinaryService.class})
 class DeleteJourneyResourceTest {
     @Autowired
     private MockMvc mockMvc;
     @Autowired
     private JourneyRepository journeyRepository;
-    @MockBean
+    @Autowired
     private CloudinaryService cloudinaryService;
-
-    @BeforeEach
-    void setup() {
-        journeyRepository.deleteAll();
-    }
 
     @Test
     void context() {
@@ -57,27 +56,26 @@ class DeleteJourneyResourceTest {
     @Test
     @WithMockUser(username = "test-user", password = "test-password", authorities = {AUTHENTICATED_USER})
     void delete_whenAuthenticatedUserOwnedJourney_shouldAllowDeletion() throws Exception {
-        journeyRepository.save(JOURNEY_EXTENDED_ENTITY);
+        when(journeyRepository.findById(any())).thenReturn(Optional.of(JOURNEY_EXTENDED_ENTITY));
 
         mockMvc.perform(MockMvcRequestBuilders.delete(Resources.DELETE_JOURNEY, JOURNEY_EXTENDED_ENTITY.getId()))
                 .andDo(print())
                 .andExpect(status().isOk());
+
+        verify(journeyRepository).deleteById(anyString());
         verify(cloudinaryService).deleteJourney(any());
-        Optional<JourneyEntity> optionalJourney = journeyRepository.findById(JOURNEY_EXTENDED_ENTITY.getId());
-        assertThat(optionalJourney).isEmpty();
     }
 
     @Test
     @WithMockUser(username = "test-admin", password = "test-password", authorities = {MAINTAINER})
-    void delete_whenLoggedInUserDoesNotAccessToJourney_shouldThrowError() throws Exception {
-        journeyRepository.save(JOURNEY_EXTENDED_ENTITY);
+    void delete_whenLoggedInUserDoesNotAccessToJourney_shouldGracefullyIgnoreRequest() throws Exception {
+        when(journeyRepository.findById(any())).thenReturn(Optional.of(JOURNEY_EXTENDED_ENTITY));
 
         mockMvc.perform(MockMvcRequestBuilders.delete(Resources.DELETE_JOURNEY, JOURNEY_EXTENDED_ENTITY.getId()))
                 .andDo(print())
                 .andExpect(status().isOk());
+        verify(journeyRepository, never()).deleteById(anyString());
         verify(cloudinaryService, never()).deleteJourney(any());
-        Optional<JourneyEntity> optionalJourney = journeyRepository.findById(JOURNEY_EXTENDED_ENTITY.getId());
-        assertThat(optionalJourney).isNotEmpty();
     }
 
     @Test
@@ -86,39 +84,36 @@ class DeleteJourneyResourceTest {
         JourneyEntity journey = JOURNEY_EXTENDED_ENTITY.toBuilder()
                 .visibilities(Set.of(Visibility.MYSELF, Visibility.MAINTAINER))
                 .build();
-        journeyRepository.save(journey);
+        when(journeyRepository.findById(any())).thenReturn(Optional.of(journey));
 
         mockMvc.perform(MockMvcRequestBuilders.delete(Resources.DELETE_JOURNEY, journey.getId()))
                 .andDo(print())
                 .andExpect(status().isOk());
+        verify(journeyRepository).deleteById(anyString());
         verify(cloudinaryService).deleteJourney(any());
-        Optional<JourneyEntity> optionalJourney = journeyRepository.findById(journey.getId());
-        assertThat(optionalJourney).isEmpty();
     }
 
     @Test
     @WithMockUser(username = "test-user", password = "test-password", authorities = {GUEST_USER})
     void delete_whenLoggedInUserDoesNotAuthority_shouldThrowError() throws Exception {
-        journeyRepository.save(JOURNEY_EXTENDED_ENTITY);
+        when(journeyRepository.findById(any())).thenReturn(Optional.of(JOURNEY_EXTENDED_ENTITY));
 
         mockMvc.perform(MockMvcRequestBuilders.delete(Resources.DELETE_JOURNEY, JOURNEY_EXTENDED_ENTITY.getId()))
                 .andDo(print())
                 .andExpect(status().isForbidden());
+        verify(journeyRepository, never()).deleteById(anyString());
         verify(cloudinaryService, never()).deleteJourney(any());
-        Optional<JourneyEntity> optionalJourney = journeyRepository.findById(JOURNEY_EXTENDED_ENTITY.getId());
-        assertThat(optionalJourney).isNotEmpty();
     }
 
     @Test
-    void delete_whenUSerNotAuthenticated_shouldThrowError() throws Exception {
-        journeyRepository.save(JOURNEY_EXTENDED_ENTITY);
+    void delete_whenUserNotAuthenticated_shouldThrowError() throws Exception {
+        when(journeyRepository.findById(any())).thenReturn(Optional.of(JOURNEY_EXTENDED_ENTITY));
 
         mockMvc.perform(MockMvcRequestBuilders.delete(Resources.DELETE_JOURNEY, JOURNEY_EXTENDED_ENTITY.getId()))
                 .andDo(print())
                 .andExpect(status().isUnauthorized());
+        verify(journeyRepository, never()).deleteById(anyString());
         verify(cloudinaryService, never()).deleteJourney(any());
-        Optional<JourneyEntity> optionalJourney = journeyRepository.findById(JOURNEY_EXTENDED_ENTITY.getId());
-        assertThat(optionalJourney).isNotEmpty();
     }
 
 }
