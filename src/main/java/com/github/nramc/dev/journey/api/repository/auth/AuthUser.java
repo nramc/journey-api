c38d1ab0cfe41cc768c50f6d1a65dd4d1a9eaeda
Ram <ramachandrannellai@gmail.com>
2024-10-02 22:23:26 +0200

    feat: clean architecture refactoring - ApplicationProperties moved to core

--- src/main/java/com/github/nramc/dev/journey/api/repository/auth/AuthUser.java
+++ src/main/java/com/github/nramc/dev/journey/api/repository/auth/AuthUser.java
@@ -48,21 +48,6 @@ public class AuthUser implements UserDetails {
     }
 
     @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
     public boolean isEnabled() {
         return enabled;
     }
