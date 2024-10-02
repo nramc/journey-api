59e2a1aabc9015f68ee59834341dab9c4a1231e3
Ram <ramachandrannellai@gmail.com>
2024-10-02 22:16:54 +0200

    feat: clean architecture refactoring - automated user creation removed since it is risk and converted to migration rule

---
+++ src/main/java/com/github/nramc/dev/journey/api/migration/user/GuestUserMigrationRule.java
@@ -0,0 +1,38 @@
+package com.github.nramc.dev.journey.api.migration.user;
+
+import com.github.nramc.dev.journey.api.core.user.security.Role;
+import com.github.nramc.dev.journey.api.repository.auth.AuthUser;
+import com.github.nramc.dev.journey.api.web.resources.rest.auth.AuthUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Slf4j
+public class GuestUserMigrationRule implements Runnable {
+    private final AuthUserDetailsService authUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run() {
+        UserDetails user = AuthUser.builder()
+                .username("GUEST")
+                .password(passwordEncoder.encode(""))
+                .roles(Set.of(Role.GUEST_USER))
+                .name("Guest")
+                .enabled(true)
+                .createdDate(LocalDateTime.now())
+                .build();
+
+        if (authUserDetailsService.userExists(user.getUsername())) {
+            log.info("app user[{}] already exists in database.", user.getUsername());
+        } else {
+            authUserDetailsService.createUser(user);
+            log.info("app user[{}] created", user.getUsername());
+        }
+    }
+}
