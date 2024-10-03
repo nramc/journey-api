3129ee1a7273cdb6efd0b03240d7075a1b8929bf
Ram <ramachandrannellai@gmail.com>
2024-10-03 14:12:05 +0200

    feat: clean architecture refactoring - unused location repository and its entity removed

--- src/main/java/com/github/nramc/dev/journey/api/repository/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.nramc.dev.journey.api.repository;
-
-import com.github.nramc.dev.journey.api.repository.entity.LocationEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LocationRepository extends MongoRepository<LocationEntity, String> {
-}
