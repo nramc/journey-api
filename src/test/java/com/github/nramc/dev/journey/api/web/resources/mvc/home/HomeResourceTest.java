15117ebf33bbd3c766affae52c5c40789f6076ed
Ram <ramachandrannellai@gmail.com>
2024-10-03 14:24:02 +0200

    feat: clean architecture refactoring - unused location repository and its entity removed

--- src/test/java/com/github/nramc/dev/journey/api/web/resources/mvc/home/HomeResourceTest.java
+++ src/test/java/com/github/nramc/dev/journey/api/web/resources/mvc/home/HomeResourceTest.java
@@ -1,13 +1,11 @@
 package com.github.nramc.dev.journey.api.web.resources.mvc.home;
 
-import com.github.nramc.dev.journey.api.core.app.ApplicationProperties;
 import com.github.nramc.dev.journey.api.config.security.WebSecurityConfig;
-import com.github.nramc.dev.journey.api.repository.LocationRepository;
+import com.github.nramc.dev.journey.api.core.app.ApplicationProperties;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,7 +18,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Import({WebSecurityConfig.class})
 @ActiveProfiles({"prod", "test"})
 @EnableConfigurationProperties({ApplicationProperties.class})
-@MockBean({LocationRepository.class})
 class HomeResourceTest {
     @Autowired
     private MockMvc mvc;
