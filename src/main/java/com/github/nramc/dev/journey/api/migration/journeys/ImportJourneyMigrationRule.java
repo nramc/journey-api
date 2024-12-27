d29d33e87b82a686cca6ed70a9382a3b2dddefce
Ram <ramachandrannellai@gmail.com>
2024-12-27 20:09:07 +0100

    feat: journey import migration rule added

---
+++ src/main/java/com/github/nramc/dev/journey/api/migration/journeys/ImportJourneyMigrationRule.java
@@ -0,0 +1,35 @@
+package com.github.nramc.dev.journey.api.migration.journeys;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.nramc.dev.journey.api.repository.journey.JourneyEntity;
+import com.github.nramc.dev.journey.api.repository.journey.JourneyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+public class ImportJourneyMigrationRule implements Runnable {
+    private static final String IMPORT_FILE_NAME = "/Users/ramachandrannellaiyappan/Ram/lab/Journey-API/output.json";
+    private final JourneyRepository journeyRepository;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void run() {
+        File file = new File(IMPORT_FILE_NAME);
+        try {
+            List<JourneyEntity> journeys = objectMapper.readValue(file, new TypeReference<>() {
+            });
+            journeyRepository.saveAll(journeys);
+            log.info("Successfully imported to {} journeys from {}", journeys.size(), file.getAbsolutePath());
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+
+}
