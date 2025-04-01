9c03984248941547020b4ed00362a017e05797ef
Ram <ramachandrannellai@gmail.com>
2025-04-01 21:58:26 +0200

    feat: add integration test application and configuration for testing

---
+++ journey-api-tests/src/test/java/com/github/nramc/dev/journey/api/tests/application/config/TestContainerConfig.java
@@ -0,0 +1,8 @@
+package com.github.nramc.dev.journey.api.tests.application.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestContainerConfig {
+
+}
