ae5acb6983a515e35b318e73c5d34e78d1f86256
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2025-05-13 22:07:42 +0200

    feat: add Checkstyle suppression for line length in SecurityHeadersCustomizer

--- journey-api-web/src/main/java/com/github/nramc/dev/journey/api/config/security/SecurityHeadersCustomizer.java
+++ journey-api-web/src/main/java/com/github/nramc/dev/journey/api/config/security/SecurityHeadersCustomizer.java
@@ -11,6 +11,7 @@ import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 @RequiredArgsConstructor
 public class SecurityHeadersCustomizer implements Customizer<HeadersConfigurer<HttpSecurity>> {
 
+    @SuppressWarnings("checkstyle:LineLength")
     @Override
     public void customize(HeadersConfigurer<HttpSecurity> headersConfigurer) {
         headersConfigurer.permissionsPolicyHeader(permissions -> permissions.policy("accelerometer=(),ambient-light-sensor=(),autoplay=(),battery=(),camera=(),display-capture=(),document-domain=(),encrypted-media=(),fullscreen=(self),gamepad=(),geolocation=(),gyroscope=(),layout-animations=(self),legacy-image-formats=(self),magnetometer=(),microphone=(),midi=(),oversized-images=(self),payment=(),picture-in-picture=(),publickey-credentials-stats=(),speaker-selection=(),sync-xhr=(self),unoptimized-images=(self),unsized-media=(self),usb=(),screen-wake-lock=(),web-share=(),xr-spatial-tracking=()"));
