ef02ca03c3a7ecf12acabb095d5fae84c90942ca
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2025-06-08 23:52:55 +0200

    feat: create home page with basic HTML structure and remove unused application properties

--- journey-api-web/src/main/java/com/github/nramc/dev/journey/api/web/resources/mvc/home/HomeResource.java
+++ journey-api-web/src/main/java/com/github/nramc/dev/journey/api/web/resources/mvc/home/HomeResource.java
@@ -1,10 +1,8 @@
 package com.github.nramc.dev.journey.api.web.resources.mvc.home;
 
-import com.github.nramc.dev.journey.api.core.app.ApplicationProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import static com.github.nramc.dev.journey.api.web.resources.Resources.HOME;
@@ -12,11 +10,9 @@ import static com.github.nramc.dev.journey.api.web.resources.Resources.HOME;
 @Controller
 @RequiredArgsConstructor
 public class HomeResource {
-    private final ApplicationProperties applicationProperties;
 
     @GetMapping(value = HOME, produces = MediaType.TEXT_HTML_VALUE)
-    public String home(Model model) {
-        model.addAttribute("applicationProperties", applicationProperties);
+    public String home() {
         return "home";
     }
 
