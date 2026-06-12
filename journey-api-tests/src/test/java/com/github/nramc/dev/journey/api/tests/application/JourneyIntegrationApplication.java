bd157aa8778c61174cd23e09e28344842cd31970
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2026-06-12 16:24:51 +0200

    feat: add ApplicationRunner to shift time by one day in JourneyIntegrationApplication

--- journey-api-tests/src/test/java/com/github/nramc/dev/journey/api/tests/application/JourneyIntegrationApplication.java
+++ journey-api-tests/src/test/java/com/github/nramc/dev/journey/api/tests/application/JourneyIntegrationApplication.java
@@ -2,7 +2,12 @@ package com.github.nramc.dev.journey.api.tests.application;
 
 import com.github.nramc.dev.journey.api.JourneyApiApplication;
 import com.github.nramc.dev.journey.api.tests.application.config.IntegrationApplicationConfig;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.modulith.moments.support.TimeMachine;
+
+import java.time.Duration;
 
 public class JourneyIntegrationApplication {
 
@@ -11,4 +16,11 @@ public class JourneyIntegrationApplication {
                 .with(IntegrationApplicationConfig.class)
                 .run(args);
     }
+
+    @Bean
+    public ApplicationRunner init(TimeMachine timeMachine) {
+        return args -> timeMachine.shiftBy(Duration.ofDays(1));
+
+    }
+
 }
