28dbc14ccc441b2f0c63229077c8671384eeef88
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2026-07-03 18:18:18 +0200

    feat: implement password recovery use case with One-Time-Token email functionality

--- journey-api-web/src/main/java/com/github/nramc/dev/journey/api/infrastructure/security/WebSecurityConfig.java
+++ journey-api-web/src/main/java/com/github/nramc/dev/journey/api/infrastructure/security/WebSecurityConfig.java
@@ -54,6 +54,7 @@ import static com.github.nramc.dev.journey.api.shared.web.Resources.HEALTH_CHECK
 import static com.github.nramc.dev.journey.api.shared.web.Resources.HOME;
 import static com.github.nramc.dev.journey.api.shared.web.Resources.LOGIN;
 import static com.github.nramc.dev.journey.api.shared.web.Resources.LOGIN_MFA;
+import static com.github.nramc.dev.journey.api.shared.web.Resources.LOGIN_OTT;
 import static com.github.nramc.dev.journey.api.shared.web.Resources.MY_SECURITY_ATTRIBUTE_EMAIL;
 import static com.github.nramc.dev.journey.api.shared.web.Resources.MY_SECURITY_ATTRIBUTE_TOTP;
 import static com.github.nramc.dev.journey.api.shared.web.Resources.MY_SECURITY_ATTRIBUTE_TOTP_STATUS;
@@ -62,6 +63,7 @@ import static com.github.nramc.dev.journey.api.shared.web.Resources.MY_SECURITY_
 import static com.github.nramc.dev.journey.api.shared.web.Resources.NEW_JOURNEY;
 import static com.github.nramc.dev.journey.api.shared.web.Resources.PROMETHEUS;
 import static com.github.nramc.dev.journey.api.shared.web.Resources.REST_DOC;
+import static com.github.nramc.dev.journey.api.shared.web.Resources.SEND_ACCOUNT_RECOVERY;
 import static com.github.nramc.dev.journey.api.shared.web.Resources.SEND_EMAIL_CODE;
 import static com.github.nramc.dev.journey.api.shared.web.Resources.SIGNUP;
 import static com.github.nramc.dev.journey.api.shared.web.Resources.TTS_API;
@@ -94,6 +96,7 @@ public class WebSecurityConfig {
     );
 
     @Bean
+    @SuppressWarnings("java:S4502") // CSRF protection is not needed for stateless REST APIs
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http
                 // configure CORS security
@@ -144,6 +147,10 @@ public class WebSecurityConfig {
                         .requestMatchers(POST, "/webauthn/authenticate/*").permitAll()
                         .requestMatchers(POST, LOGIN).authenticated()
                         .requestMatchers(POST, LOGIN_MFA).authenticated()
+                        .requestMatchers(POST, LOGIN_OTT).permitAll()
+
+                        // Account recovery resources
+                        .requestMatchers(POST, SEND_ACCOUNT_RECOVERY).permitAll()
 
                         .requestMatchers(GET, FIND_JOURNEYS).access(readAccessAuthorizationManager)
                         .requestMatchers(GET, FIND_JOURNEY_BY_ID).access(readAccessAuthorizationManager)
