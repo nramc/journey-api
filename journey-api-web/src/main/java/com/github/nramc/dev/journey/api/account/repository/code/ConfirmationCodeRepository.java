ead8285d4dbb32fb730f1683b1e7fdf2035a81bf
Ramachandran Nellaiyappan <ramachandrannellai@gmail.com>
2026-07-03 18:16:24 +0200

    feat: implement password recovery use case with One-Time-Token email functionality

--- journey-api-web/src/main/java/com/github/nramc/dev/journey/api/account/repository/code/ConfirmationCodeRepository.java
+++ journey-api-web/src/main/java/com/github/nramc/dev/journey/api/account/repository/code/ConfirmationCodeRepository.java
@@ -1,11 +1,17 @@
 package com.github.nramc.dev.journey.api.account.repository.code;
 
+import com.github.nramc.dev.journey.api.shared.domain.user.security.ConfirmationCodeType;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ConfirmationCodeRepository extends MongoRepository<ConfirmationCodeEntity, String> {
 
     List<ConfirmationCodeEntity> findAllByUsername(String username);
+
+    Optional<ConfirmationCodeEntity> findByCodeAndType(String code, ConfirmationCodeType type);
+
+    void deleteAllByUsernameAndType(String username, ConfirmationCodeType type);
 
 }
